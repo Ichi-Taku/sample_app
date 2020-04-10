62979b37e577d53b106303ab8e00698269fa7404
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-04-10 15:33:01 +0900

    create_activation_digestのprivate化

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,14 +43,15 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  # メールアドレスをすべて小文字にする
-  def downcase_email
-    self.email = email.downcase
-  end
+  private
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
 
-  # 有効化トークンとダイジェストを作成および代入する
-  def create_activation_digest
-    self.activation_token  = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
