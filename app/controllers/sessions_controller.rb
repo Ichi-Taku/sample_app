8523c4c6bcb2ae63566151b90738d1288cda3339
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-04-10 18:14:51 +0900

    有効化のテストとリファクタリング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,14 @@ class SessionsController < ApplicationController
     #debugger
   end
 
+  #9.3.1.1よりuserがインスタンス変数になっている
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
