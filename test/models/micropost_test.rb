48e1023e125d7805b718a30c869e478c1114a751
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-04-14 11:01:07 +0900

    UserとMicropostの関連付け

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,9 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
     # このコードは正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    #@micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
+
   end
 
   test "should be valid" do
