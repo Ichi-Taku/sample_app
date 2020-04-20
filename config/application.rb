da286b3c8f24e11d60f9a123028bdc69458ef78c
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-04-20 18:16:58 +0900

    static_pages_controller_specの作成

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+=begin 
     config.generators do |g|
       g.test_framework :rspec,
                        helper_specs: false,
@@ -18,7 +18,7 @@ module SampleApp
                        view_specs: false,
                        controller_specs: false
     end
-    
+=end    
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
