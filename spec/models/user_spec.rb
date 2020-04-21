17bf9c746c54a8b72c1c54dcc6a043ed150d7514
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-04-21 18:56:33 +0900

    Usersモデルの名前とメールアドレスのバリデーションテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,34 @@ RSpec.describe User, type: :model do
 
 
   describe `validations` do
-    it {is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_length_of(:name).is_at_most(50) }
+    it { is_expected.to validate_length_of(:email).is_at_most(255) }
+    it do
+      is_expected.to allow_values('first.last@foo.jp',
+                                  'user@example.com',
+                                  'USER@foo.COM',
+                                  'A_US-ER@foo.bar.org',
+                                  'alice+bob@baz.cn').for(:email)
+    end
+    it do
+      is_expected.to_not allow_values('user@example,com',
+                                      'user_at_foo.org',
+                                      'user.name@example.',
+                                      'foo@bar_baz.com',
+                                      'foo@bar+baz.com').for(:email)
+    end
+    describe `validate unique email` do
+      let!(:user) {create(:user, email: "duplicatemail@example.com")}
+      it `is invalid with a duplicate email` do
+        user = build(:user, email: 'duplicatemail@example.com')
+        expect(user).to_not be_valid
+      end
+      it 'is case insensitive in email' do
+        user = build(:user, email: 'ORIGINAL@EXAMPLE.COM')
+        expect(user).to be_valid
+      end
+    end
   end
 end
