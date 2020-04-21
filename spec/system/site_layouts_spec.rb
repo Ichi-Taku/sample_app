64603ecb95f3cd742e89113274eb61630640672c
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-04-21 17:28:33 +0900

    UsersControllerのnewメソッドのテスト

--- spec/system/site_layouts_spec.rb
+++ spec/system/site_layouts_spec.rb
@@ -1,25 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "SiteLayouts", type: :system do
-  before do
-    visit(root_path)
-  end
+  include ApplicationHelper
   context `access to root_path` do
-    
+    before do
+      visit(root_path)
+    end
     it `has root_path` do
-      expect(page).to have_link nil, href: root_path, count: 2
+      expect(page).to have_link , href: root_path, count: 2
     end
 
     it `has help_path` do
-      expect(page).to have_link nil, href: help_path
+      expect(page).to have_link "Help", href: help_path
     end
 
     it `has about_path` do
-      expect(page).to have_link nil, href: about_path
+      expect(page).to have_link "About", href: about_path
     end
 
     it `has contact_path` do
-      expect(page).to have_link nil, href: contact_path
+      expect(page).to have_link "Contact", href: contact_path
+    end
+  end
+
+  context `access to signup_path` do
+    before do
+      visit(signup_path)
+    end
+    it `has "Sign up" title` do
+      expect(page).to have_title full_title('Sign up')
     end
   end
 end
