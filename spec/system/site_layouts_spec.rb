94db83aedf2aa522541f309c63b0194ef7a261b0
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-04-21 15:37:34 +0900

    homeにあるリンクのテスト

--- spec/system/site_layouts_spec.rb
+++ spec/system/site_layouts_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "SiteLayouts", type: :system do
-  #context 
+  before do
+    visit(root_path)
+  end
+  context `access to root_path` do
+    
+    it `has root_path` do
+      expect(page).to have_link nil, href: root_path, count: 2
+    end
+
+    it `has help_path` do
+      expect(page).to have_link nil, href: help_path
+    end
+
+    it `has about_path` do
+      expect(page).to have_link nil, href: about_path
+    end
+
+    it `has contact_path` do
+      expect(page).to have_link nil, href: contact_path
+    end
+  end
 end
