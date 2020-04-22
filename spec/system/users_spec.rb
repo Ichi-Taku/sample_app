8be970002d94deae4d0aed206abdb54e54f2cb49
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-04-22 16:32:43 +0900

    signup時のviewのRspecの修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,12 +7,13 @@ RSpec.describe "Users", type: :system do
         visit signup_path
         fill_in 'Name', with: 'testuser'
         fill_in 'Email', with: 'testuser@example.com'
+        fill_in 'UserID', with: 'testuserid'
         fill_in 'Password', with: 'password'
-        fill_in 'Confirmation', with: 'password'
+        fill_in 'Password confirmation', with: 'password'
         click_button 'Create my account'
       end
       it 'gets an flash message' do
-        expect(page).to have_selector('.alert-success', text: 'Welcome to the Sample App!')
+        expect(page).to have_selector('.alert-info', text: 'Please check your email to activate your account.')
       end
     end
     context 'enter an invalid values' do
@@ -20,15 +21,16 @@ RSpec.describe "Users", type: :system do
         visit signup_path
         fill_in 'Name', with: ''
         fill_in 'Email', with: ''
+        fill_in 'UserID', with: ''
         fill_in 'Password', with: ''
-        fill_in 'Confirmation', with: ''
+        fill_in 'Password confirmation', with: ''
         click_button 'Create my account'
       end
       subject { page }
       it 'gets an errors' do
         is_expected.to have_selector('#error_explanation')
-        is_expected.to have_selector('.alert-danger', text: 'The form contains 6 errors.')
-        is_expected.to have_content("Password can't be blank", count: 2)
+        is_expected.to have_selector('.alert-danger', text: 'The form contains 5 errors.')
+        is_expected.to have_content("Password can't be blank")
       end
       it 'render to /signup url' do
         is_expected.to have_current_path '/signup'
